d4fb60a7bbb2117086a5d077eb76353f2a9255f4
Sarah Razanatsoa <sarah.raz@onja.org>
2020-10-11 11:02:32 +0300

    disable the shadow with the outlines buttons

--- buttons.js
+++ buttons.js
@@ -4,14 +4,14 @@ function Button(props) {
     let classUsed = "";
     if(props.variant && props.disabled) {
         classUsed = `${props.variant} disabled` 
+    } else if(props.variant) {
+        classUsed = `${props.variant} `
     } else if(props.color && props.size) {
         classUsed = `${props.color} ${props.size}`
     } else if(props.startIcon && props.color) {
         classUsed = `${props.startIcon} ${props.color}`
     } else if(props.endIcon && props.color) {
         classUsed = `${props.endIcon} ${props.color}`
-    } else if(props.variant) {
-        classUsed = `${props.variant} `
     } else if(props.disabledShadow && props.color) {
         classUsed = `${props.color} disabledShadow`
     }
